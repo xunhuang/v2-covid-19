aa7884a1ce10a65b11cd909bdf02813ed54b5b08
Xun Wilson Huang <xhuang@gmail.com>
2021-10-08 13:32:41 -0700

    init db

--- docker-db/initdb.sh
+++ docker-db/initdb.sh
@@ -13,9 +13,6 @@ b="\
  us_hospitalization(date,adult_icu_bed_used,\"inIcuCurrently\",\"hospitalizedCurrently\",adult_icu_beds_capacity,inpatient_beds_used,inpatient_beds_capacity)^official.us-hospitalization.csv\
  states_testing(date,\"totalTestResults\",\"negativeIncrease\",\"positiveIncrease\",state_name,negative,\"totalTestResultsIncrease\",positive,inconclusive,\"inconclusiveIncrease\",state,state_fips_code)^official.states-testing.csv\
  us_testing(date,\"totalTestResults\",\"negativeIncrease\",\"positiveIncrease\",negative,positive,inconclusive,\"inconclusiveIncrease\")^official.us-testing.csv\
- states_summary(state_name,state_fips_code,confirmed_cases,confirmed_increase,confirmed_increase_14days,\"totalTestResults\",\"totalTestResultsIncrease\",positive,\"positiveIncrease\",\"inIcuCurrently\",\"hospitalizedCurrently\",adult_icu_beds_capacity,inpatient_beds_used,inpatient_beds_capacity)^official.states-summary.csv\
- us_summary(\"totalTestResults\",\"negativeIncrease\",\"positiveIncrease\",negative,positive,inconclusive,\"inconclusiveIncrease\",adult_icu_bed_used,\"inIcuCurrently\",\"hospitalizedCurrently\",adult_icu_beds_capacity,inpatient_beds_used,inpatient_beds_capacity,confirmed_cases,confirmed_increase,confirmed_increase_14days)^official.us-summary.csv\
- counties_summary(state_fips_code,county_fips_code,state,county,confirmed_cases,deaths,test_7day_total,positivity_7day_avg,community,school,report_date,healthwebsites)^my_dataset.covid-19-county-official-view.csv\
 "
 
 CONN=postgres://postgres:mysecretpassword@localhost/postgres
@@ -27,13 +24,6 @@ init_schema_meta () {
    psql -Atx $CONN -c "create database postgres"
    CONN=postgres://postgres:mysecretpassword@localhost/postgres
    psql -Atx $CONN < meta/init_data.sql
-   # psql -Atx $CONN < data/fips_code_state.sql
-   # psql -Atx $CONN < data/fips_code_county.sql
-   # psql -Atx $CONN < data/county_population.sql
-   # psql -Atx $CONN < data/state_population.sql
-   # psql -Atx $CONN < data/county_geo.sql
-   # psql -Atx $CONN <  data/msa.sql
-   # psql -Atx $CONN <  data/msa_counties.sql
 }
 
 init_data() {
