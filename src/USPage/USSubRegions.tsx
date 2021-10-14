2fc67c0affd2bb1ef8d746c01b92d69d2a86938a
Xun Wilson Huang <xhuang@gmail.com>
2021-10-14 15:18:55 -0700

    get rid of undesirable graphs

--- src/USPage/USSubRegions.tsx
+++ src/USPage/USSubRegions.tsx
@@ -21,51 +21,53 @@ export const USSubRegions = () => {
   }
   const nodes = data?.allStateSummaryViews?.nodes;
 
-  const sorted_nodes = [...nodes!].sort((a, b) => {
-    const da =
-      a?.stateCasesAllsByStateFipsCode.nodes[0]?.confirmedCases! -
-      a?.stateCasesAllsByStateFipsCode.nodes[30]?.confirmedCases!;
-    const db =
-      b?.stateCasesAllsByStateFipsCode.nodes[0]?.confirmedCases! -
-      b?.stateCasesAllsByStateFipsCode.nodes[30]?.confirmedCases!;
-    return db / b?.population! - da / a?.population!;
-  });
+  const sorted_nodes = [...nodes!]
+    .sort((a, b) => {
+      const da =
+        a?.stateCasesAllsByStateFipsCode.nodes[0]?.confirmedCases! -
+        a?.stateCasesAllsByStateFipsCode.nodes[30]?.confirmedCases!;
+      const db =
+        b?.stateCasesAllsByStateFipsCode.nodes[0]?.confirmedCases! -
+        b?.stateCasesAllsByStateFipsCode.nodes[30]?.confirmedCases!;
+      return db / b?.population! - da / a?.population!;
+    })
+    .filter(
+      (state) =>
+        state?.stateCasesAllsByStateFipsCode.nodes &&
+        state?.stateCasesAllsByStateFipsCode.nodes.length > 50
+    );
 
   return (
     <GraphContainer>
       {sorted_nodes?.map((state) => (
         <GraphSingle>
-          {state?.stateCasesAllsByStateFipsCode.nodes &&
-            state?.stateCasesAllsByStateFipsCode.nodes.length > 50 && (
-              <AdvancedGraph
-                title={state?.stateName!}
-                subtitle={`Avg ${Math.round(
-                  ((state?.stateCasesAllsByStateFipsCode.nodes[0]
-                    ?.confirmedCases! -
-                    state?.stateCasesAllsByStateFipsCode.nodes[30]
-                      ?.confirmedCases!) /
-                    state.population! /
-                    30) *
-                    100000
-                )}/100K/day past month`}
-                serieses={[
-                  {
-                    series: DataSeries.fromGraphQLQueryNodes(
-                      "Confirmed",
-                      state?.stateCasesAllsByStateFipsCode.nodes! as object[],
-                      "confirmedCases"
-                    )
-                      .change()
-                      .setLabel("New Cases"),
-                    color: "green",
-                    covidspecial: true,
-                    showMovingAverage: true,
-                  },
-                ]}
-                showControls={false}
-                initNumberOfDays={180}
-              />
-            )}
+          <AdvancedGraph
+            title={state?.stateName!}
+            subtitle={`Avg ${Math.round(
+              ((state?.stateCasesAllsByStateFipsCode.nodes[0]?.confirmedCases! -
+                state?.stateCasesAllsByStateFipsCode.nodes[30]
+                  ?.confirmedCases!) /
+                state?.population! /
+                30) *
+                100000
+            )}/100K/day past month`}
+            serieses={[
+              {
+                series: DataSeries.fromGraphQLQueryNodes(
+                  "Confirmed",
+                  state?.stateCasesAllsByStateFipsCode.nodes! as object[],
+                  "confirmedCases"
+                )
+                  .change()
+                  .setLabel("New Cases"),
+                color: "green",
+                covidspecial: true,
+                showMovingAverage: true,
+              },
+            ]}
+            showControls={false}
+            initNumberOfDays={180}
+          />
         </GraphSingle>
       ))}
     </GraphContainer>
