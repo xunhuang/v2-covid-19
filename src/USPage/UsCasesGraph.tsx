6504e13acddffa81119f2ff96a300b8224bff0d0
Xun Wilson Huang <xhuang@gmail.com>
2021-12-09 12:59:05 -0800

    fix bug

--- src/USPage/UsCasesGraph.tsx
+++ src/USPage/UsCasesGraph.tsx
@@ -5,7 +5,7 @@ export const UsCasesGraph = () => {
   const { data, loading } = useUsCasesQuery();
   const cases = data?.allUsCasesAlls?.nodes as Array<CasesObject>;
   let fixed = cases?.filter((entry) => entry.date !== "2021-11-29");
-  fixed.push({
+  fixed?.push({
     date: "2021-11-29",
     confirmedCases: 48411494,
     deaths: 779293,
