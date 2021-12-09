b8e57fe80adc3993700c7b054f63b417204ce32c
Xun Wilson Huang <xhuang@gmail.com>
2021-12-09 15:47:53 -0800

    reverse the gui hack

--- src/USPage/UsCasesGraph.tsx
+++ src/USPage/UsCasesGraph.tsx
@@ -4,17 +4,11 @@ import { CasesGraph, CasesObject } from './CasesGraph';
 export const UsCasesGraph = () => {
   const { data, loading } = useUsCasesQuery();
   const cases = data?.allUsCasesAlls?.nodes as Array<CasesObject>;
-  let fixed = cases?.filter((entry) => entry.date !== "2021-11-29");
-  fixed?.push({
-    date: "2021-11-29",
-    confirmedCases: 48411494,
-    deaths: 779293,
-  });
 
   return (
     <div>
       {loading && <div>loading</div>}
-      {cases && <CasesGraph cases={fixed} />}
+      {cases && <CasesGraph cases={cases} />}
     </div>
   );
 };
